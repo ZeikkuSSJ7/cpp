631ac9f78a72331d0ed8319ad89cfb6a39d45d74
ZeikkuSSJ7 <47304153+ZeikkuSSJ7@users.noreply.github.com>
2019-03-26 13:04:46 +0100

    Finishing strings

---
+++ pointers_arrays_strings/strings.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    // using cin.getline to introduce a whole line
+    char string [50];
+    cout << "Introduce a line: ";
+    cin.getline(string, 50);
+    cout << "You introduced: " << string << "\n";
+
+    // using string comparison
+    char word [9];
+    char another_word [9];
+
+    cout << "Introduce mountain: ";
+    cin.getline(word, 9, '\n');
+
+    cout << "Introduce sun: ";
+    cin.getline(another_word, 9, '\n');
+
+    cout << word << " " << another_word << "\n";
+    cout << strcmp(word, another_word) << "\n";
+    
+    // string concat
+    char concat [18];
+    cout << strcat(strcat(concat, word), another_word) << "\n";
+
+    // string copy
+    strcpy(word, another_word);
+    cout << word << "\n";
+    
+    return 0;
+}
